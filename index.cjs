a35a78c922369f55cd7c5a55b163cc77f1441fa7
4026112@gmail.com <4026112@gmail.com>
2023-05-03 22:54:32 +0300

    Add project files.

---
+++ index.cjs
@@ -0,0 +1,31 @@
+ const express =require('express');
+ const cors =require( 'cors');
+ const app=express();
+ app.use(cors());
+ const port=3000;
+ app.listen(port,()=>{
+  console.log(`example app listening on https://localhost:${port}`);
+ })
+
+
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://api.render.com/v1/services?limit=20',
+  headers: {
+    accept: 'application/json',
+    authorization: 'Bearer rnd_qrKDhzWln7p8TvItJKTvSWtKBWGS'
+  }
+};
+
+app.get('/', (req,res)=>{
+axios
+  .request(options)
+  .then(function (response) {
+    res.send(response.data);
+  })
+  .catch(function (error) {
+    res.send(error);
+  });
+ })
